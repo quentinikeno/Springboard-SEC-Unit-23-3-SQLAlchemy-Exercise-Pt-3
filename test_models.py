309d1c71b605ce29ad67723f275fefee822ec065
Quentin Ikeno <ikenoq@gmail.com>
2022-03-09 19:57:43 -1000

    Create file to test models.

---
+++ test_models.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+from app import app
+from models import db, User
+
+# Use test database and don't clutter tests with SQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pet_shop_test'
+app.config['SQLALCHEMY_ECHO'] = False
+
+db.drop_all()
+db.create_all()
+
+class UserModelTestCase(TestCase):
+    """Tests for model for Users."""
+    
+    def setUp(self):
+        """Clean up any existing users."""
+        User.query.delete()
+        
+    def tearDown(self):
+        """Clean up any failed transactions."""
+        db.sessionl.rollback()
