5a4ba0fd5485ea90f3e7c84a6c6c36791e73a7e3
Quentin Ikeno <ikenoq@gmail.com>
2022-03-09 18:22:34 -1000

    Add route and form to edit users

--- app.py
+++ app.py
@@ -29,16 +29,16 @@ def show_users():
     return render_template('users.html', users=users)
 
 @app.route('/users/new')
-def create_user():
+def show_new_user_form():
     """Render form to create new user."""
     return render_template('new_user_form.html')
 
 @app.route('/users/new', methods=['POST'])
 def create_user():
     """Post route to create new user and add to database."""
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    image_url = request.form['image_url']
+    first_name = request.form['first-name']
+    last_name = request.form['last-name']
+    image_url = request.form['image-url'] or None
     
     new_user = User(first_name = first_name, last_name = last_name, image_url = image_url)
     db.session.add(new_user)
@@ -46,8 +46,14 @@ def create_user():
     
     return redirect(f'/{new_user.id}')
 
-@app.route('/<int:user_id>')
-def show_user(user_id):
+@app.route('/users/<int:user_id>')
+def show_user_detail(user_id):
     """Show details about a single user."""
     user = User.query.get_or_404(user_id)
     return render_template('user_detail.html', user=user)
+
+@app.route('/users/<int:user_id>/edit')
+def edit_user_detail(user_id):
+    """Edit details about a single user."""
+    user = User.query.get_or_404(user_id)
+    return render_template('edit_user_form.html', user=user)
