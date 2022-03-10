f61f223f3361a8f16bbe8e6569b14b50023a525e
Quentin Ikeno <ikenoq@gmail.com>
2022-03-09 21:23:42 -1000

    Added docstrings to unit tests.

--- test_flask.py
+++ test_flask.py
@@ -37,6 +37,7 @@ class UserViewsTestCase(TestCase):
         db.session.rollback()
         
     def test_list_users(self):
+        """Test the home route and listing users."""
         with app.test_client() as client:
             resp = client.get('/', follow_redirects=True)
             html = resp.get_data(as_text=True)
@@ -45,6 +46,7 @@ class UserViewsTestCase(TestCase):
             self.assertIn('Test User', html)
             
     def test_show_user(self):
+        """Test showing a user."""
         with app.test_client() as client:
             resp = client.get(f"/users/{self.user_id}")
             html = resp.get_data(as_text=True)
@@ -54,6 +56,7 @@ class UserViewsTestCase(TestCase):
             self.assertIn("https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=735&amp;q=80", html)
             
     def test_add_user(self):
+        """Test adding user."""
         with app.test_client() as client:
             data = {"first-name": "FirstTest2", "last-name": "LastTest2", "image-url": "https://images.unsplash.com/photo-1491609154219-ffd3ffafd992?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}
             resp = client.post("/users/new", data=data, follow_redirects=True)
@@ -63,7 +66,8 @@ class UserViewsTestCase(TestCase):
             self.assertIn("<h1>FirstTest2 LastTest2</h1>", html)
 
     def test_edit_user(self):
-        with app.test_client() as client:
+        """Test editing a user."""
+        with app.test_client() as client:    
             data = {"first-name": "EditTest", "last-name": "EditUser", "image-url": "https://images.unsplash.com/photo-1491609154219-ffd3ffafd992?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}
             resp = client.post(f"/users/{self.user_id}/edit", data=data, follow_redirects=True)
             html = resp.get_data(as_text=True)
