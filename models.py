188a0b944a282c112da96fbde42692a0878bc024
Quentin Ikeno <ikenoq@gmail.com>
2022-03-10 20:48:16 -1000

    Create Post model

--- models.py
+++ models.py
@@ -8,16 +8,15 @@ def connect_db(app):
     db.init_app(app)
     
 class User(db.Model):
-    """Users Model"""
+    """Users model"""
+    
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    
     first_name = db.Column(db.String(50), nullable = False)
-    
     last_name = db.Column(db.String(50), nullable = False)
-    
-    image_url = db.Column(db.Text, nullable = False, default = "https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80")
+    image_url = db.Column(db.Text, nullable = False, 
+                        default = "https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80")
     
     def __repr__(self):
         """Representation of User Instance"""
@@ -28,3 +27,20 @@ class User(db.Model):
     def full_name(self):
         """Show first and last names concatenated together."""
         return f"{self.first_name} {self.last_name}"
+    
+class Post(db.Model):
+    """Posts model"""
+    __tablename__ = 'posts'
+    
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    title = db.Column(db.String(40), nullable = False)
+    content = db.Column(db.Text, nullable = False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    users = db.relationship('User', backref = 'posts')
+    
+    def __repr__(self):
+        """Representation of Post Instance"""
+        p = self
+        return f"<Post id={p.id} title={p.title} created_at={p.created_at} user_id={p.user_id}>"
